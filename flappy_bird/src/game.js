f003265dffc8b88d5e44c5dd88285cd65537944e
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-05-20 17:22:34 -0400

    rendering solved for flappy the bird

--- flappy_bird/src/game.js
+++ flappy_bird/src/game.js
@@ -1,6 +1,17 @@
+import Level from "./level";
+
 export default class FlappyBird {
   constructor(canvas){
     this.ctx = canvas.getContext("2d");
     this.dimensions = { width: canvas.width, height: canvas.height };
+    this.animate();
+  }
+
+  animate(){
+    this.level = new Level(this.dimensions)
+    this.level.drawBackground(this.ctx)
+    // level.drawBackground(window)
   }
 }
+
+
